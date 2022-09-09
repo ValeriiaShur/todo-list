ba1d8a588af7cbf8861f2b057cbdc6430706c4bd
Valeriia Shurupina <valeriia@igne.nl>
2022-09-09 16:12:33 +0200

    create and style main page view

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import { useToDoStore } from '../../data/stores/useToDoStore';
+import { InputPlus } from '../components/InputPlus';
+import { InputTask } from '../components/InputTask';
+import styles from './index.module.scss';
 
 export const App: React.FC = () => {
+    const [
+        tasks,
+        createTask,
+        updateTask,
+        removeTask
+    ] = useToDoStore(state => [
+        state.tasks,
+        state.createTask,
+        state.updateTask,
+        state.removeTask,
+    ]);
+
+
     return (
-        <article>121231</article>
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>ToDo App</h1>
+            <section className={styles.articleSection}>
+                <InputPlus 
+                    onAdd={(title) => {
+                        if (title) {
+                            createTask(title)
+                        }
+                    }}    
+                />
+            </section>
+            <section className={styles.articleSection}>
+                {!tasks.length && (
+                  <p className={styles.articleText}>There are no tasks added yet.</p>
+                )}
+                {tasks.map((task) => (
+                  <InputTask
+                    key={task.id}
+                    id={task.id}
+                    title={task.title}
+                    onDone={removeTask}
+                    onEdited={updateTask}
+                    onRemoved={removeTask}
+                  />
+                ))}
+            </section>
+        </article>
     )
 };
