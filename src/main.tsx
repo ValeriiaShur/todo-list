f09dd42e3b77bbff81fe9dc8588f967d4aaa8ee1
Valeriia Shurupina <valeriia@igne.nl>
2022-10-17 10:15:38 +0200

    make mobile view nicer

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './views/styles/reset.scss';
 import './views/styles/common.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
